500e8569e2a2e33b5ec117f200546bfec025cb43
Arthur Nishikawa <nishikawa.poli@gmail.com>
2018-10-18 14:40:57 +0200

    Fix muC information

--- animacoes/mu23e3.sh
+++ animacoes/mu23e3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 animation_cpartition.py coupled_FoFo_375_mu23e3 -t 0 100 .1 -y -.3 2 -m -T '$\mu_C = 23,3$ kJ/mol'
+python3 animation_cpartition.py coupled_FoFo_375_mu23e3 -t 0 100 .1 -y -.3 2 -m -T '$\mu_C = 23.2$ kJ/mol'
